8959ad2cd690a4c099988c38d6f2d87f84e649ec
houstonfloyd <andrew.houstonfloyd@gmail.com>
2017-03-13 19:17:58 -0700

    Update

--- todo.rb
+++ todo.rb
@@ -5,6 +5,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "sinatra/content_for"
 
 configure do
 	enable :sessions
@@ -43,6 +44,21 @@ get "/lists/new" do
 	erb :new_list, layout: :layout
 end
 
+#Return an error message if the name is invalid. Otherwise will return nil
+def error_for_list_name(name)
+	if !(1..100).cover? name.size #.include? would check every single value in range
+		"List name must be between 1 and 100 characters."
+	elsif session[:lists].any? { |list| list[:name] == name }
+		"List name must be unique."
+	end
+end
+
+def error_for_todo(name)
+	if !(1..100).cover? name.size #.include? would check every single value in range
+		"List name must be between 1 and 100 characters."
+	end
+end
+
 # Create a new list
 ## Note how we redirect when a valid action takes place, but instead render
 ## a page when there is some sort of error. This is due to stateless HTTP -
@@ -52,14 +68,76 @@ end
 ## we would lose access to data related to the current request (see below)
 post "/lists" do
 	list_name = params[:list_name].strip
-	if (1..100).cover? list_name.size #.include? would check every single value in range
+
+	error = error_for_list_name(list_name) #removed this from if statement
+	if error #more explicit and easier to read
+		session[:error] = error
+		erb :new_list, layout: :layout
+	else
 		session[:lists] << { name: list_name, todos: [] }
 		session[:success] = "The list has been created."
-		redirect "/lists"
-	else
-		session[:error] = "List name must be between 1 and 100 characters."
 		## i.e. if I redirected here, I would lose access to list_name var, or 
-		## params[:list_name]
-		erb :new_list, layout: :layout
+		## params[:list_name]		
+		redirect "/lists"
 	end
+end
+
+get "/lists/:id" do
+	@list_id = params[:id].to_i
+	@list = session[:lists][@list_id]
+	erb :list
+end
+
+get "/lists/:id/edit" do
+	id = params[:id].to_i
+	@list = session[:lists][id]
+	erb :edit_list, layout: :layout
+end
+
+#Update an existing todo list
+post "/lists/:id" do
+	list_name = params[:list_name].strip
+	@list_id = params[:id].to_i
+	@list = session[:lists][@list_id]
+
+	error = error_for_list_name(list_name) #removed this from if statement
+	if error #more explicit and easier to read
+
+		session[:error] = error
+		erb :edit_list, layout: :layout
+	else
+		@list[:name] = list_name
+		session[:success] = "The list has been updated."
+		## i.e. if I redirected here, I would lose access to list_name var, or 
+		## params[:list_name]		
+		redirect "/lists/#{id}"
+	end
+end
+
+post "/lists/:id/delete" do
+	id = params[:id].to_i
+	session[:lists].delete_at(id)
+	session[:success] = "The list has been deleted."
+	redirect "/lists"
+end
+
+post "/lists/:list_id/todos" do
+	@list_id = params[:list_id].to_i
+	@list = session[:lists][@list_id]
+	text = params[:todo].strip
+
+	error = error_for_todo(text)
+	if error
+		session[:error] = error
+		erb :list, layout: :layout
+	else
+		@list[:todos] << { name: text, completed: false }
+		session[:success] = "The todo was added."
+		redirect "/lists/#{@list_id}"
+	end
+end
+
+post "/lists/:list_id/todos/:id/delete" do
+	@todo_id = params[:todo_id].to_i
+	@list = session[:lists][@list_id]
 end
